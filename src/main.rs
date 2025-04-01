d9abc713016cf815c032545c387f924dac026669
4lrick <alrick.rubio@epitech.eu>
2025-03-31 20:24:31 -0400

    [ADD] Entry and Button in a VBox

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Box, Button, Entry};
+use gtk4 as gtk;
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id("org.spt.simplepackagetracker")
+        .build();
+
+    app.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(800)
+            .default_height(600)
+            .title("Simple Package Tracker")
+            .build();
+
+        let vbox = Box::new(gtk4::Orientation::Vertical, 15);
+
+        let text_field = Entry::builder()
+            .placeholder_text("Enter up to 50 tracking numbers, each per line")
+            .build();
+
+        let button = Button::with_label("Track");
+        button.connect_clicked(|_| {
+            // TODO: print the buffer of the input field
+            eprintln!("Clicked!");
+        });
+
+        vbox.append(&text_field);
+        vbox.append(&button);
+        window.set_child(Some(&vbox));
+
+        window.present();
+    });
+
+    app.run()
 }
+
