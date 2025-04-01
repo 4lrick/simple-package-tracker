87f510641ca8d62744ba1fe59f4807bd9a61432f
4lrick <alrick.rubio@epitech.eu>
2025-04-01 11:29:28 -0400

    [ADD] Parse multiple lines

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Box, Button, Entry};
-use gtk4 as gtk;
+use gtk::{glib, Application, ApplicationWindow, Box, Button, Label, TextView};
+use gtk4::{self as gtk};
 
 fn main() -> glib::ExitCode {
     let app = Application::builder()
@@ -17,18 +17,34 @@ fn main() -> glib::ExitCode {
 
         let vbox = Box::new(gtk4::Orientation::Vertical, 15);
 
-        let text_field = Entry::builder()
-            .placeholder_text("Enter up to 50 tracking numbers, each per line")
-            .build();
+        let text_field = TextView::builder().bottom_margin(100).build();
 
+        let text_field_cloned = text_field.clone();
+        let tracking_label = Label::builder().build();
+        let tracking_label_cloned = tracking_label.clone();
         let button = Button::with_label("Track");
-        button.connect_clicked(|_| {
-            // TODO: print the buffer of the input field
-            eprintln!("Clicked!");
+
+        button.connect_clicked(move |_| {
+            let tf_buff = text_field_cloned.buffer();
+            let text_field_output = tf_buff.text(&tf_buff.start_iter(), &tf_buff.end_iter(), false);
+            let mut tracking_number = Vec::new();
+            for line in text_field_output.lines() {
+                let is_clean: bool =
+                    line.trim().chars().all(|c| c.is_ascii_digit()) && !line.is_empty();
+                if !is_clean {
+                    continue;
+                }
+
+                let message = format!("Tracking: {}", line);
+                tracking_number.push(message);
+            }
+            tracking_label_cloned.set_text(&tracking_number.join("\n"));
+            println!("{}", tracking_number.join("\n"));
         });
 
         vbox.append(&text_field);
         vbox.append(&button);
+        vbox.append(&tracking_label);
         window.set_child(Some(&vbox));
 
         window.present();
@@ -36,4 +52,3 @@ fn main() -> glib::ExitCode {
 
     app.run()
 }
-
